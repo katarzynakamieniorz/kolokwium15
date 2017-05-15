3c0470fd45f5f14e2d8ef3cbf0700ee636818b0d
katarzynakamieniorz <asleep@onet.eu>
2017-05-15 15:21:54 +0200

    ProgramKolokwium15.20

---
+++ ProgramKolokwium15.20/ProgramKolokwium/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramKolokwium
+{
+    class Program
+    {
+
+        int PierwszaW, DrugaW;
+
+        public void UstawPunkt ()
+        {
+            PierwszaW = 0;
+            DrugaW = 0;
+                        
+        }
+
+        public int ObliczBokProstokata ()
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            String WybranaFigura;
+            
+            
+
+
+
+            Console.WriteLine("T - trapez prostokatny\nO - okrag\nP - prostokat");
+            WybranaFigura = Console.ReadLine();
+
+            if(WybranaFigura=="t")
+            {
+                Console.WriteLine("Podaj pierwsza wspolrzedna punktu zaczepienia");
+                Console.ReadLine();
+                Console.WriteLine("Podaj druga wspolrzedna punktu zaczepienia");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ ProgramKolokwium15.20/Testy/TestyJed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testy
+{
+    [TestClass]
+    public class TestyJed
+    {
+        [TestMethod]
+        public void TestUstawWspolrzedna()
+        {
+            int liczbatestowa = 1;
+
+            int PierwszaWsp = Program.PierwszaW;
+            int DrugaWsp = Program.DrugaW;
+
+            Assert.AreEqual("1", PierwszaWsp);
+            Assert.AreEqual("1", DrugaWsp);
+
+        }
+
+        [TestMethod]
+        public int TestObliczBok (int bok)
+        {
+
+            return bok;
+        }
+
+        
+            
+            
+    }
+}
