c4ccca6ab45c3519a42d7bc580628924f44f9931
katarzynakamieniorz <asleep@onet.eu>
2017-05-15 15:12:10 +0200

    ProgramKolokwium

--- ProgramKolokwium/ProgramKolokwium/Program.cs
+++ ProgramKolokwium/ProgramKolokwium/Program.cs
@@ -8,8 +8,37 @@ namespace ProgramKolokwium
 {
     class Program
     {
+
+        public void UstawPunkt (int PierwszaWspolrzedna, int DrugaWspolrzedna)
+        {
+            
+        }
+
+        public int ObliczBokProstokata ()
+        {
+            return 0;
+        }
+
+
         static void Main(string[] args)
         {
+
+            String WybranaFigura;
+            int PierwszaW, DrugaW;
+
+
+
+            Console.WriteLine("T - trapez prostokatny\nO - okrag\nP - prostokat");
+            WybranaFigura = Console.ReadLine();
+
+            if(WybranaFigura=="t")
+            {
+                Console.WriteLine("Podaj pierwsza wspolrzedna punktu zaczepienia");
+                Console.ReadLine();
+                Console.WriteLine("Podaj druga wspolrzedna punktu zaczepienia");
+                Console.ReadLine();
+
+            }
         }
     }
 }
---
+++ ProgramKolokwium/Testy/TestyJed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testy
+{
+    [TestClass]
+    public class TestyJed
+    {
+        [TestMethod]
+        public void TestUstawWspolrzedna()
+        {
+            int liczbatestowa = 1;
+
+            int wyjscie = Program.PierwszaW;
+
+            Assert.AreEqual("1", wyjscie);
+
+        }
+
+        [TestMethod]
+        public int TestMethod2 (int bok)
+        {
+
+            return bok;
+        }
+            
+            
+            
+            
+            
+    }
+}
