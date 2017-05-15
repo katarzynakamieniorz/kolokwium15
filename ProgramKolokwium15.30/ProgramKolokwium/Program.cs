5428be82852896e4bad13eebffa372c580a9391c
katarzynakamieniorz <asleep@onet.eu>
2017-05-15 15:33:26 +0200

    ProgramKolokwium15.30

---
+++ ProgramKolokwium15.30/ProgramKolokwium/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramKolokwium
+{
+ 
+
+    class Program
+    {
+        int PierwszaW, DrugaW;
+        int BokPierwszy, BokDrugi;
+
+        public void UstawPunkt()
+        {
+            Console.WriteLine("Podaj pierwsza wspolrzedna punktu zaczepienia");
+            PierwszaW = Int32.Parse(Console.ReadLine());
+        }
+
+        public void UstawPunkty()
+        {
+            Console.WriteLine("Podaj pierwsza wspolrzedna punktu zaczepienia");
+            PierwszaW = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga wspolrzedna punktu zaczepienia");
+            DrugaW = Int32.Parse(Console.ReadLine());
+        }
+
+        public int ObliczBokProstokata()
+        {
+            return 0;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            String WybranaFigura;
+            int PierwszaW, DrugaW;
+            int BokPierwszy, BokDrugi;
+
+
+
+
+            Console.WriteLine("T - trapez prostokatny\nO - okrag\nP - prostokat");
+            WybranaFigura = Console.ReadLine();
+
+            if(WybranaFigura=="t")
+            {
+                ProgramKolokwium.Program.UstawPunkty();
+
+            }
+        }
+    }
+}
---
+++ ProgramKolokwium15.30/Testy/TestyJed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testy
+{
+    [TestClass]
+    public class TestyJed
+    {
+        [TestMethod]
+        public void TestUstawWspolrzedna()
+        {
+            int liczbatestowa = 1;
+
+            int PierwszaWsp = ProgramKolokwium.Program.PierwszaW;
+            int DrugaWsp = ProgramKolokwium.Program.DrugaW;
+
+            Assert.AreEqual(liczbatestowa, PierwszaWsp);
+            Assert.AreEqual(liczbatestowa, DrugaWsp);
+
+        }
+
+        [TestMethod]
+        public void TestObliczBok (int bok)
+        {
+            bok = 0;
+
+            int BokTestowany = ProgramKolokwium.Program.BokPierwszy;
+
+            Assert.AreEqual(bok, BokTestowany);
+
+        }
+
+      
+            
+            
+            
+    }
+}
